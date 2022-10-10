de4fac661a92b218c8f443583f4e0abb6b12b3ed
Hasanul Bari <hasanulbari8@gmail.com>
2022-10-11 00:13:04 +0600

    loaddata

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const quizData = useLoaderData();
+    const quizs = quizData.data;
+    console.log(quizs)
     return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
